9ee9c2dc73f43aec427b1a8c6991b72e6cd3fb79
Donnis Kent <dkent@southeasternequip.com>
2021-12-15 12:09:53 -0500

    added day 4

---
+++ day4/adventDay4.py
@@ -0,0 +1,12 @@
+#1. Pull in the string of calls, put them in a list 
+#2. Pull in each board. 
+    #- Create a 2x2 matrix for each board of size 5x5
+    #- scan a line, add it to the first row of the matrix, scan a line, etc 
+
+drawnNumbers = input().split(",")
+
+
+
+    
+
+   
